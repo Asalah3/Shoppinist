3465fed5e4a238d49c1903778b46dd7b14fb4177
Soha Ahmed Hamdy <soha7hmed@gmail.com>
2023-06-06 08:18:55 +0300

    Draft order API calls and favorite view Model

---
+++ Shoppinist/Modules/FavouriteModule/Model/APIModel/DraftOrderNetworkService.swift
@@ -0,0 +1,128 @@
+//
+//  DraftOrderNetworkService.swift
+//  Shoppinist
+//
+//  Created by Soha Ahmed Hamdy on 06/06/2023.
+//
+
+import Foundation
+
+protocol DraftNetworkProtocol{
+    static func CreateDraft(product: Product, complication:@escaping (Int) -> Void)
+    static func deleteDraft(draftID: Int, complication:@escaping (Int) -> Void)
+    static func getAllDraftOrders( completionHandeler: @escaping ((AllDrafts?), Error?) -> Void)
+}
+class DraftNetwork:DraftNetworkProtocol{
+    static func deleteDraft(draftID: Int, complication: @escaping (Int) -> Void) {
+        let url = URL(string: "https://47f947d8be40bd3129dbe1dbc0577a11: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@mad-ism-43-1.myshopify.com/admin/api/2023-04/draft_orders/\(draftID).json")
+        var urlRequest = URLRequest(url: url!)
+        urlRequest.httpMethod = "DELETE"
+        urlRequest.httpShouldHandleCookies = false
+        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            if (data != nil && data?.count != 0){
+                if let httpResponse = response as? HTTPURLResponse {
+                    let response = String(data:data!,encoding: .utf8)
+                     print(response!)
+                    complication(httpResponse.statusCode)
+                    
+                   }
+            }
+            
+        }.resume()
+        
+        
+        
+        
+    }
+    
+    static func getAllDraftOrders(completionHandeler: @escaping ((AllDrafts?), Error?) -> Void) {
+        let url = URL(string: "https://47f947d8be40bd3129dbe1dbc0577a11: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@mad-ism-43-1.myshopify.com/admin/api/2023-04/draft_orders.json")
+        guard let newUrl = url else {
+            return
+        }
+        print("newUrl\(newUrl)")
+        let request = URLRequest(url: newUrl)
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: request){ data ,response , error in
+            do{
+                let result = try JSONDecoder().decode(AllDrafts.self, from: data ?? Data())
+                completionHandeler(result, nil)
+                print("success in getDrafts")
+
+
+            }catch let error{
+                print(error.localizedDescription)
+                print("error in getDrafts")
+                completionHandeler(nil, error)
+            }
+            
+        }
+        task.resume()
+    }
+    
+
+    
+    static func CreateDraft(product: Product, complication:@escaping (Int) -> Void) {
+
+        let url = URL(string: "https://47f947d8be40bd3129dbe1dbc0577a11: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@mad-ism-43-1.myshopify.com/admin/api/2023-04/draft_orders.json")
+        var urlRequest = URLRequest(url: url!)
+        urlRequest.httpMethod = "POST"
+        let userDictionary = [
+            "draft_order": [
+              "line_items": [
+                [
+                    "id": product.id!,
+                    "title": product.title!,
+                    "invoice_url": product.image!,
+                    "quantity": 1,
+                    "price": (product.variants?[0].price!)!
+                ]
+              ],
+              
+              "applied_discount": [
+                "description": "Custom discount",
+                "value": "10.0",
+                "title": "Custom",
+                "amount": "10.00",
+                "value_type": "fixed_amount"
+              ],
+              
+              "customer": [
+                "id": UserDefaults.standard.integer(forKey:"customerID"),
+                
+                "default_address": [
+                  
+                  "default": true
+                ]
+              ]
+            ]
+          ]
+
+        urlRequest.httpShouldHandleCookies = false
+        do {
+            
+            let bodyDictionary = try JSONSerialization.data(withJSONObject: userDictionary,options: .prettyPrinted)
+            urlRequest.httpBody = bodyDictionary
+            urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            if (data != nil && data?.count != 0){
+                if let httpResponse = response as? HTTPURLResponse {
+                    let response = String(data:data!,encoding: .utf8)
+                     print(response!)
+                    complication(httpResponse.statusCode)
+                    
+                   }
+            }
+            
+        }.resume()
+        
+        
+        
+        
+    }
+    
+}
+
