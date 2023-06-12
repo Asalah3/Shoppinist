64340eab9f5aa7928559db11c5dbfa514ea1720a
Asalah3 <asalahsayed3@gmail.com>
2023-06-12 22:55:06 +0300

    AddedFilterByPriceInBrandProducts

--- Shoppinist/Modules/BrandProductsModule/ViewModel/BrandProductsViewModel.swift
+++ Shoppinist/Modules/BrandProductsModule/ViewModel/BrandProductsViewModel.swift
@@ -15,14 +15,15 @@ class BrandProductsViewModel : BrandProductsViewModelProtocol{
         self.remote = remoteDataSource
     }
     var fetchProductsToBrandProductsViewController : (()->())={}
-    var fetchProductsData:ProductModel!{
+    var fetchProductsData:[Product]!{
         didSet{
             fetchProductsToBrandProductsViewController()
         }
     }
     func fetchBrandProducts(collectionId :Int) {
         remote?.fetchBrandProducts(collection_id: "\(collectionId)"){ result in
-            guard let result = result else {return}
+            let sortedList = result?.products?.sorted(by:{Double($0.variants?[0].price ?? "0.0") ?? 0.0 < Double($1.variants?[0].price ?? "0.0") ?? 0.0})
+            guard let result = sortedList else {return}
             self.fetchProductsData = result
         }
     }
