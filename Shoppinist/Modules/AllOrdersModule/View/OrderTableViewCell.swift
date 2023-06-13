85700eae29477e74dd1bddc734023bad634bc494
Asalah3 <asalahsayed3@gmail.com>
2023-06-13 23:28:49 +0300

    Added All Orders Logic

--- Shoppinist/Modules/AllOrdersModule/View/OrderTableViewCell.swift
+++ Shoppinist/Modules/AllOrdersModule/View/OrderTableViewCell.swift
@@ -14,7 +14,6 @@ class OrderTableViewCell: UITableViewCell {
     @IBOutlet weak var orderShippedTo: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
