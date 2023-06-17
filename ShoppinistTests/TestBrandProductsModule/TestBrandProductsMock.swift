2dc23ddf940c38e44005a40d5f67ec272e7565fe
Asalah3 <asalahsayed3@gmail.com>
2023-06-17 23:32:12 +0300

    Added BrandProductsTesting

---
+++ ShoppinistTests/TestBrandProductsModule/TestBrandProductsMock.swift
@@ -0,0 +1,22 @@
+//
+//  TestBrandProductsMock.swift
+//  ShoppinistTests
+//
+//  Created by Asalah Sayed on 17/06/2023.
+//
+
+import XCTest
+@testable import Shoppinist
+final class TestBrandProductsMock: XCTestCase {
+
+    func testRootDataDecoding(){
+        BrandProductsMock().fetchBrandProducts(collection_id: "8355031417124") {res in
+            guard let result = res else{
+                XCTFail()
+                return
+            }
+            XCTAssertNotNil(result)
+        }
+    }
+
+}
